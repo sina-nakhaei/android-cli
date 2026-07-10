e2057da20350f4022e7a4141dba5f18b36a773af
Sina Nakhaei <cna.nakhaei@gmail.com>
2026-07-10 11:22:27 +0330

    feat: automate release versioning with buildconfig and actions

--- src/main/kotlin/AndroidCommand.kt
+++ src/main/kotlin/AndroidCommand.kt
@@ -1,7 +1,19 @@
 package com.github.sinanakhaei
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.versionOption
+import com.github.sinanakhaei.android_cli.BuildConfig
 
-class AndroidCommand : CliktCommand(name = "android") {
+class AndroidCommand : CliktCommand(
+    name = "android",
+) {
+    init {
+        versionOption(
+            version = BuildConfig.APP_VERSION, // This is now dynamically injected by Gradle!
+            names = setOf("-v", "--version"),
+            message = { "android-cli version $it" }
+        )
+    }
+
     override fun run() {}
 }
